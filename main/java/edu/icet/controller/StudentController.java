1eff273b0c9f8d9c0209ddda6ff41ec103d9ed2e
Yasiru Kavinda <yasiru0210@gmail.com>
2024-08-17 09:31:36 +0530

    third l2

--- main/java/edu/icet/controller/StudentController.java
+++ main/java/edu/icet/controller/StudentController.java
@@ -2,8 +2,8 @@ package edu.icet.controller;
 
 import edu.icet.dto.Student;
 import edu.icet.service.StudentService;
-import edu.icet.service.StudentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -12,16 +12,22 @@ import java.util.List;
 @RestController
 @CrossOrigin
 public class StudentController {
-         @Autowired
-         StudentService service;
 
-         @GetMapping("/get-data")
-         public List<Student>getData(){
+    protected final StudentService service;
 
-             return service.getData();
-         }
-         @PostMapping  ("/add-data")
-         public void addData(@RequestBody Student student){
-             service.addData(student);
+    @Autowired
+    public StudentController(@Qualifier("studentServiceImplv3") StudentService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/get-data")
+    public List<Student> getData() {
+        return service.getData();
+    }
+
+    @PostMapping("/add-data")
+    public void addData(@RequestBody Student student) {
+        service.addData(student);
     }
 }
+
